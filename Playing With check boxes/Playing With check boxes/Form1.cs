a68a4c98036aa7ee3676521446ca9137e8387d86
Brxn16 <camplese123@gmail.com>
2018-11-30 12:26:33 -0600

    update

---
+++ Playing With check boxes/Playing With check boxes/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Playing_With_check_boxes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                label1.Text = "Check Box 1 is checked";
+            }
+            else if (checkBox2.Checked)
+            {
+                label1.Text = "Check Box 2 is checked";
+            }
+            else if (checkBox3.Checked)
+            {
+                label1.Text = "Check Box 3 is checked";
+            }
+            if (checkBox2.Checked && checkBox3.Checked)
+            {
+                label1.Text = "Check Box 2 and 3 are checked";
+            }
+
+        }
+    }
+}
